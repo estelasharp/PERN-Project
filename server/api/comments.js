44c6e4eaa53062c1f2c249cc140c85be2e168d74
Estela <estela.sharp@yahoo.com>
2024-01-23 23:07:33 -0500

    updated notes

--- server/api/comments.js
+++ server/api/comments.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { getCommentsByMap, createComment, updateComment, deleteComment } = require('../dataBase/helperFunctions/commentshelper')
 
-//GET - /api/maps/:mapId - get comments by map 
+// GET - /api/maps/:mapId - get comments by map 
 router.get('/:mapId', async (req, res, next) => {
     try {
         const comments = await getCommentsByMap(req.params.mapId)
@@ -12,7 +12,7 @@ router.get('/:mapId', async (req, res, next) => {
     }
 })
 
-//POST - /api/comments - create new comment
+// POST - /api/comments - create new comment
 router.post('/', async (req, res, next) => {
     try {
         // include title, body (text), date, mapid, resortid, in req.body 
@@ -26,7 +26,7 @@ router.post('/', async (req, res, next) => {
 })
 
 
-//PUT - /api/comments/:id
+// PUT - /api/comments/:id
 router.put('/:id', async (req, res, next) => {
     try {
         const comment = await updateComment(req.params.id, req.body)
@@ -37,7 +37,7 @@ router.put('/:id', async (req, res, next) => {
 })
 
 
-//DELETE - /api/comments/:id - 
+// DELETE - /api/comments/:id - 
 router.delete('/:id', async (req, res, next) => {
     try {
         const comment = await deleteComment(req.params.id)
